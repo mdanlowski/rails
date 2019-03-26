980aa38b325eedaaf68869a7476d017028855bcb
mdanlowski <marcindanlowski@gmail.com>
2019-03-26 10:36:43 +0100

    add forgotten validations to feed model

--- rss_feed_aggr/app/models/feed.rb
+++ rss_feed_aggr/app/models/feed.rb
@@ -1,6 +1,15 @@
 class Feed < ApplicationRecord
   has_many :entries, dependent: :destroy
 
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :url, presence: true
+
+  validates :update_interval, presence: true, numericality: {
+    only_integer: true,
+    greater_than: 0,
+  }
+
   validates :keep_n_last, numericality: {
     only_integer: true,
     greater_than: 0,
